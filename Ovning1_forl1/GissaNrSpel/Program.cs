e8a6681094e34b89f15fe88c7d8874fda3f21ccd
Sudden <christiansundin@gmail.com>
2019-09-10 18:48:31 +0100

    Added ovning1

---
+++ Ovning1_forl1/GissaNrSpel/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+
+namespace GissaNrSpel
+{
+  
+  static class Game
+  {
+    static ArrayList gameHistory = new ArrayList();
+    static void Main( string[] args )
+    {
+      int choice = -1;
+      Console.WriteLine("Welcome to the game!");
+      while (choice != 0)
+      {
+        choice = Menu();
+        if (choice == 1)
+          PlayGame();
+      }
+      Console.WriteLine("Game statistics (nr geuesses): " );
+      foreach (int i in gameHistory)
+      {
+        Console.Write(i + " ");
+      }
+      Console.WriteLine();
+    }
+    static int Menu()
+    {
+      char val = '.';
+      while(!(val == 'Y' || val == 'N' || val == 'y' || val == 'n') )     //If it is not a valid reply, ask again
+      {
+        Console.WriteLine("Play game?: Y/N");
+        val = Console.ReadKey().KeyChar;
+        Console.WriteLine();
+      }
+      if (val == 'n' || val == 'N')
+        return 0;
+      else
+        return 1;
+    }
+    static void PlayGame()
+    {
+      int currentNr = 0;
+      Random rand = new Random();
+      int currentGuess = -1;
+      int nrGuesses = 0;
+      currentNr = rand.Next(1, 101);
+      Console.WriteLine("Guess the number! ");
+      while(currentGuess!=currentNr)
+      {
+        nrGuesses++;
+        try
+        {
+          currentGuess = Convert.ToInt32(Console.ReadLine());
+        }
+        catch
+        {
+          Console.WriteLine("Invalid number, try again.");
+          continue;
+        }
+        if( currentGuess > currentNr)
+        {
+          Console.WriteLine("Too high, try again.");
+        }
+        else if (currentGuess < currentNr)
+        {
+          Console.WriteLine("Too low, try again.");
+        }
+      }
+      Console.WriteLine("Congratulations! The the number was: {0} and the number of guesses was: {1}", currentNr, nrGuesses);
+      gameHistory.Add(nrGuesses);
+    }
+  }
+}
---
+++ Ovning1_forl1/Ovning1_forl1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Ovning1_forl1
+{
+  class Program
+  {
+    static void Main( string[] args )
+    {
+      Console.WriteLine("Vad heter du?");
+      string name = Console.ReadLine();
+      Console.WriteLine("Vad är din favoriträtt?");
+      string food = Console.ReadLine();
+      Console.WriteLine("Hej," + name + ", kul att du gillar " + food + " det gör jag med");
+    }
+  }
+}
